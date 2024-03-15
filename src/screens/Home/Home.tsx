d7fc43c9294e0340132710f6fa92ee56f44ba50e
Lucas Silva <nascimentolucas786@gmail.com>
2024-03-15 13:36:28 -0300

    Atualizando a screen Home

--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -1,15 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { Text, View } from 'react-native';
+import { View } from 'react-native';
 import styles from './Style'
-import { Menu } from '../../components/Menu/Menu';
+import { BatLogo } from '../../components/BatLogo/BatLogo';
+import { BatButton } from '../../components/BatButton/BatButton';
 
 export default function Home() {
   return (
-    <View style={styles.container}>
-      <Menu/>
-      <Text>Testing Screen!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.appContainer}>
+
+      <View style={styles.logoContainer}>
+        <BatLogo/>
+      </View>
+
+      <View style={styles.inputContainer}>
+        <BatButton/>
+      </View>
+
+
+      <StatusBar style="light" />
     </View>
   );
 }
